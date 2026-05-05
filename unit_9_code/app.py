e231b2e13d795c057456e96dc4356be918538aab
ajlcoder <alissa.li@edubga.com>
2026-05-05 20:32:26 +0100

    created a data base and corercted database header names

--- unit_9_code/app.py
+++ unit_9_code/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow import fields
 import os
 
 app = Flask(__name__)
@@ -14,37 +15,35 @@ ma = Marshmallow(app)
 
 class weather_data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    districts = db.Column(db.String(100), unique=True)
+    area = db.Column(db.String(100), unique=True)
+    date = db.Column(db.String(100))
     am_1read = db.Column(db.Float)
     am_2read = db.Column(db.Float)
     pm_1read = db.Column(db.Float)
     pm_2read = db.Column(db.Float)
 
-    def __init__(self, districts, am_1read, am_2read, pm_1read, pm_2read):
-        self.districts = districts
+    def __init__(self, area, date, am_1read, am_2read, pm_1read, pm_2read):
+        self.area = area 
+        self.date = date
         self.am_1read = am_1read
         self.am_2read = am_2read
         self.pm_1read = pm_1read
         self.pm_2read = pm_2read
 
-class username(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-
-    def __init__(self, name):
-        self.name = name
 
 class weather_dataSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'districts', 'am_1read', 'am_2read', 'pm_1read', 'pm_2read')
+    area = fields.Str()
+    date = fields.Str()
+    am_1read = fields.Float()
+    am_2read = fields.Float()   
+    pm_1read = fields.Float()
+    pm_2read = fields.Float()
 
-class usernameSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'name')
 
-weather_data_schema = weather_dataSchema(strict=True)
-weather_data_schemas = weather_dataSchema(many=True, strict=True)
-username_schema = usernameSchema(strict=True)
+
+weather_data_schema = weather_dataSchema()
+weather_data_schemas = weather_dataSchema(many=True)
+
 
 
 
